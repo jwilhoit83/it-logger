5bf3940788a8a40938cbd58940007506da9fb651
Jerod Wilhoit <jwilhoit83@gmail.com>
2021-02-17 14:39:35 -0600

    added the start of a log component, added fetching of logs at component level

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import {useEffect} from 'react'
-import 'materialize-css/dist/css/materialize.min.css';
-import M from 'materialize-css/dist/js/materialize.min.js'
+import { useEffect } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
+import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
 
 const App = () => {
   useEffect(() => {
     // initializes Materialize JS
     M.AutoInit();
-  })
+  });
 
-  return <div className="App">IT Logger</div>;
+  return (
+    <>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </>
+  );
 };
 
 export default App;
